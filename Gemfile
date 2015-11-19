8d5ddcfd72ccf1b15e25bbc3b2f88783d1e9cdc0
関野 涼也 <sryoya0814@gmail.com>
2015-11-19 21:27:37 +0900

    基本問題2

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ group :development, :test do
   gem 'kaminari'
   gem 'devise'
   gem "rspec-rails", "~> 3.1.0"
+  gem 'factory_girl_rails', "~> 4.4.1"
+  gem 'faker'
 end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,34 +2,33 @@ require 'rails_helper'
 describe User  do
   describe '#create' do
     it 'is invalid if nickname is over 6 characters' do
-      user = User.new(nickname: "aaaaaaa", password: "11111111", password_confirmation: "11111111", email: "a@gmail.com" )
+      user = build(:user, nickname: "aaaaaaa")
       user.valid?
       expect(user.errors[:nickname]).to include("is too long (maximum is 6 characters)")
     end
 
     it 'is invalid without a nickname' do
-      user = User.new(nickname: "", password: "11111111", password_confirmation: "11111111", email: "a@gmail.com" )
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
     it 'is invalid without an email' do
-      user = User.new(nickname: "aaaaaa", password: "11111111", password_confirmation: "11111111", email: "" )
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
     it 'is invalid without a nickname and an email' do
-      user = User.new(nickname: "", password: "11111111", password_confirmation: "11111111", email: "" )
+      user = build(:user, nickname: "", email: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
       expect(user.errors[:email]).to include("can't be blank")
     end
 
     it 'is invalid if an email already exists' do
-    user1 = User.new(nickname: "aaaaaa", password: "11111111", password_confirmation: "11111111", email: "a@gmail.com" )
-    user1.save
-    user2 = User.new(nickname: "bbbbbb", password: "11111111", password_confirmation: "11111111", email: "a@gmail.com" )
+    user1 = create(:user)
+    user2 = build(:user, email: user1.email)
     user2.valid?
     expect(user2.errors[:email]).to include("has already been taken")
     end
